03371ad3aba7fb00c51a760c37bec4452a5774c7
Adrián Mouly <adrian@mouly.ar>
2021-10-14 16:17:41 -0300

    Configure stacks.

--- stacks/AuthStack.ts
+++ stacks/AuthStack.ts
@@ -1,7 +1,7 @@
 import * as sst from "@serverless-stack/resources";
 import { ReactStaticSite } from "@serverless-stack/resources";
 
-export default class MarketingStack extends sst.Stack {
+export default class AuthStack extends sst.Stack {
   constructor(scope: sst.App, id: string, props?: sst.StackProps) {
     super(scope, id, props);
 
--- stacks/DashboardStack.ts
+++ stacks/DashboardStack.ts
@@ -1,11 +1,11 @@
 import * as sst from "@serverless-stack/resources";
-import { ReactStaticSite } from "@serverless-stack/resources";
+import { ReactStaticSite, StaticSite } from "@serverless-stack/resources";
 
 export default class DashboardStack extends sst.Stack {
   constructor(scope: sst.App, id: string, props?: sst.StackProps) {
     super(scope, id, props);
 
-    const website = new ReactStaticSite(this, "DashboardReactSite", {
+    const website = new StaticSite(this, "DashboardReactSite", {
       path: "packages/dashboard",
       buildOutput: "dist",
       buildCommand: "npm run build",
